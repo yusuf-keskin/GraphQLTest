e2ea912714d2d330bc442227fbb493935c7d6b41
Yusuf Keskin <yusuf_keskin@outlook.com>
2023-03-26 13:00:14 +0300

    Initial Commit

---
+++ GraphQLTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GraphQLTest
+//
+//  Created by YUSUF KESKİN on 26.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
